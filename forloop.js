87eada516c9c7eaa4d876a876a6c406f2b2ae64b
Raj Shukla <rajshukla9508@gmail.com>
2023-03-12 10:18:47 +0530

    learned about folloop

---
+++ forloop.js
@@ -0,0 +1,14 @@
+// For loop
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+let total = 0;
+
+let num = 10;
+
+for (let i = 1; i <= num; i++) {
+  total = total + i;
+}
+console.log(total);
