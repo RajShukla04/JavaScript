7704a1bfa1ca1d393549c394d011b2cfb66cc9c8
Raj Shukla <rajshukla9508@gmail.com>
2023-03-12 10:20:55 +0530

    learned about switch case

---
+++ switch.js
@@ -0,0 +1,32 @@
+console.log("switch");
+
+// switch statement
+
+let Day = 3;
+
+switch (Day) {
+  case 0:
+    console.log("Sunday");
+    break;
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thrusday");
+    break;
+  case 5:
+    console.log("friday");
+    break;
+  case 6:
+    console.log("saturday");
+    break;
+  default:
+    console.log("Invailed Day");
+}
+// it's Easy
